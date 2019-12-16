83e2a307a5a33bba12e2af072dc084ae995183c3
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2019-12-17 01:18:24 +0400

    Changed db name and Added   ProducesResponseAttributes to person controllers

--- TBCPersonsDirectory.Api/Controllers/PersonsController.cs
+++ TBCPersonsDirectory.Api/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TBCPersonsDirectory.Common;
 using TBCPersonsDirectory.Common.Api;
@@ -24,6 +25,7 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(200, Type = typeof(ServiceResponse<List<PersonReadDto>>))]
         public async Task<IActionResult> Get([FromQuery]PersonSearchModel model)
         {
             var serviceResponse =  await _personsService.GetAll(model);
@@ -37,13 +39,15 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(422, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> Create(PersonCreateDto personCreateDto)
         {
             var serviceResponse = await _personsService.Create(personCreateDto);
 
             if (serviceResponse.IsSuccess)
             {
-                // todo: Created status should be here
                 return CreatedAtAction(nameof(GetById), new { id = serviceResponse.Result.Id }, serviceResponse.Result); 
             }
 
@@ -51,6 +55,8 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> GetById(int id)
         {
             var serviceResponse =  await _personsService.GetById(id);
@@ -64,6 +70,9 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> Update(int id, PersonUpdateDto personUpdateDto)
         {
             var serviceResponse =  await _personsService.Update(id, personUpdateDto);
@@ -77,6 +86,9 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPut("{id}/phones/{phone-number-id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> UpdatePhoneNumber([FromRoute] int id, [FromRoute(Name = "phone-number-id")] int phoneNumberId, PhoneNumberUpdateDto phoneNumberUpdateDto)
         {
             var serviceResponse =  await _personsService.UpdatePersonPhone(id, phoneNumberId, phoneNumberUpdateDto);
@@ -90,6 +102,9 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpDelete("{id}/phones/{phone-number-id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> RemovePhoneNumber([FromRoute]int id, [FromRoute(Name = "phone-number-id")] int phoneNumberId)
         {
             var serviceResponse =  await _personsService.RemovePersonsPhone(id, phoneNumberId);
@@ -103,6 +118,9 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPost("{id}/phones")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> AddPhonesForPerson([FromRoute]int id, PhoneNumberCreateDto phoneNumberCreateDto)
         {
             var serviceResponse =  await _personsService.AddPhoneNumber(id, phoneNumberCreateDto);
@@ -117,6 +135,10 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
+
         public async Task<IActionResult> Delete(int id)
         {
             var serviceResponse =  await _personsService.Delete(id);
@@ -129,6 +151,11 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPost("{id}/connected-persons/")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(409, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(422, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> AddConnectedPerson([FromRoute] int id, [FromBody]PersonConnectionsCreateDto relatedPersonCreateDto)
         {
             var serviceResponse =  await _personsService.CreateConnection(id, relatedPersonCreateDto);
@@ -142,6 +169,10 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpPut("{id}/connected-persons/{connected-person-id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(422, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> UpdateConnectionType(
             [FromRoute] int id,
             [FromRoute(Name = "connected-person-id")] int targetPersonId,
@@ -158,6 +189,9 @@ namespace TBCPersonsDirectory.Api.Controllers
         }
 
         [HttpDelete("{id}/connected-persons/{connected-person-id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400, Type = typeof(ServiceResponse))]
+        [ProducesResponseType(404, Type = typeof(ServiceResponse))]
         public async Task<IActionResult> RemoveConnection([FromRoute]int id, [FromRoute(Name = "connected-person-id")] int targetPersonId)
         {
             var serviceResponse =  await _personsService.RemovePersonConnection(id, targetPersonId);
@@ -169,7 +203,5 @@ namespace TBCPersonsDirectory.Api.Controllers
 
             return TransformServiceErrorToHttpStatusCode(serviceResponse.ServiceErrorMessage);
         }
-
-       
     }
 }
