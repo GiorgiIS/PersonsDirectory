98469e8e8b0acd510ff0df5faccf5f3efbcb2cfa
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2019-12-17 16:34:44 +0400

    Removed public modifier from person dto interface

--- TBCPersonsDirectory.Services/Dtos/PersonDtos/PersonDtoInterface.cs
+++ TBCPersonsDirectory.Services/Dtos/PersonDtos/PersonDtoInterface.cs
@@ -6,7 +6,7 @@ namespace TBCPersonsDirectory.Services.Dtos.PersonDtos
 {
     public interface PersonDtoInterface
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
     }
 }
